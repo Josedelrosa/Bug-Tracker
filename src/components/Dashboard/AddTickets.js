52e3dfb9c059230ae2ba8239fe4c242d22dbfa84
Jose Del Rosario <delrosa1@go.stockton.edu>
2022-07-07 01:04:14 -0400

    base template for ticket list

---
+++ src/components/Dashboard/AddTickets.js
@@ -0,0 +1,181 @@
+import * as React from "react";
+import { useState, useRef, useEffect } from "react";
+import Alert from "@mui/material/Alert";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import Checkbox from "@mui/material/Checkbox";
+import { Button } from "@mui/material";
+import { useAuth } from "../../contexts/AuthContext";
+import AddIcon from "@mui/icons-material/Add";
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const style = {
+  position: "absolute",
+  top: "20%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function AddTickets({
+  currentMembers,
+  projectId,
+  setCurrentMembers,
+}) {
+  const { allUsers, addMembers, getProjects } = useAuth();
+  const [error, setError] = useState("");
+  const [open, setOpen] = useState(false);
+  const projectNameRef = useRef();
+  const projectDescriptionRef = useRef();
+  const [checked, setChecked] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setError("");
+    setChecked([]);
+  };
+
+  const handleToggle = (value) => () => {
+    const fullname = value.firstName + " " + value.lastName;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+    console.log(checked);
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+    // console.log(value);
+    setChecked(newChecked);
+  };
+  console.log(allUsers);
+  console.log(currentMembers);
+  console.log(
+    allUsers.filter(
+      (item) =>
+        !currentMembers.some((currentMembers) => currentMembers.id === item.id)
+    )
+  );
+  // console.log(checked);
+  // console.log(checked.length);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (checked.length === 0) {
+      return setError("No members where selected");
+    }
+    const mergeResult = [...currentMembers, ...checked];
+
+    try {
+      setError("");
+      setLoading(true);
+      await addMembers(projectId, mergeResult);
+      await getProjects();
+      setCurrentMembers(mergeResult);
+      handleClose();
+    } catch (error) {
+      setError("Failed to add team members");
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <AddIcon onClick={handleOpen} />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <Box
+              component="form"
+              sx={{
+                "& .MuiTextField-root": { m: 1, width: "45ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              {error && (
+                <Alert variant="outlined" severity="error">
+                  {error}
+                </Alert>
+              )}
+              <div>
+                <h4>Add Team Members</h4>
+                <List
+                  dense
+                  sx={{
+                    width: "100%",
+                    maxWidth: "45ch",
+                    overflow: "auto",
+                    maxHeight: 200,
+                    bgcolor: "background.paper",
+                    border: 0,
+                  }}
+                >
+                  {allUsers
+                    .filter(
+                      (item) =>
+                        !currentMembers.some(
+                          (currentMembers) => currentMembers.id === item.id
+                        )
+                    )
+                    .map((value) => {
+                      const labelId = `checkbox-list-secondary-label-${value.id}`;
+                      const fullname = value.firstName + " " + value.lastName;
+                      return (
+                        <ListItem
+                          key={value.id}
+                          secondaryAction={
+                            <Checkbox
+                              edge="end"
+                              onChange={handleToggle(value)}
+                              checked={checked.indexOf(value) !== -1}
+                              inputProps={{ "aria-labelledby": labelId }}
+                            />
+                          }
+                          disablePadding
+                        >
+                          <ListItemButton>
+                            <ListItemText
+                              id={labelId}
+                              primary={`${fullname}`}
+                            />
+                          </ListItemButton>
+                        </ListItem>
+                      );
+                    })}
+                </List>
+              </div>
+              <Button
+                variant="contained"
+                type="submit"
+                disabled={loading}
+                onClick={handleSubmit}
+              >
+                Submit
+              </Button>
+            </Box>
+          </Typography>
+        </Box>
+      </Modal>
+    </React.Fragment>
+  );
+}
